e5b658a8bc9822e4ea12bdcea483dd85119b10b7
Spyros Foniadakis <spyros.foniadakis@gmail.com>
2015-07-10 19:00:32 +0200

    topicmodeler n progress

--- src/main/java/com/fda/dim/featx/TopicModeler.java
+++ src/main/java/com/fda/dim/featx/TopicModeler.java
@@ -5,7 +5,9 @@ import java.io.File;
 import java.io.FileReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Formatter;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Locale;
 import java.util.TreeSet;
@@ -30,129 +32,100 @@ import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 public class TopicModeler {
+	
+	public static InstanceList createInstanceList(){
+		// Begin by importing documents from text to feature sequences
+				ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
+
+				// String stopPath="/home/spyros/git/fda/dim/assets/stopwords.txt";
+				String stopPath = "/tudelft.net/staff-bulk/ewi/insy/mmc/nathan/dim-data/en.txt";
+				
+				// Pipes: lowercase, tokenize, remove stopwords, map to features
+				pipeList.add(new CharSequenceLowercase());
+				pipeList.add(new CharSequence2TokenSequence(Pattern
+						.compile("\\p{L}[\\p{L}\\p{P}]+\\p{L}")));
+				pipeList.add(new TokenSequenceRemoveStopwords(new File(stopPath),
+						"UTF-8", false, false, false));
+				pipeList.add(new TokenSequence2FeatureSequence());
+
+				InstanceList instances = new InstanceList(new SerialPipes(pipeList));
+				
+				return instances;
+	}
 
 	public static void main(String[] args) throws Exception {
 
-		// Begin by importing documents from text to feature sequences
-		ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
-
-		// String stopPath="/home/spyros/git/fda/dim/assets/stopwords.txt";
-		String stopPath = "/tudelft.net/staff-bulk/ewi/insy/mmc/nathan/dim-data/en.txt";
 		
-		// Pipes: lowercase, tokenize, remove stopwords, map to features
-		pipeList.add(new CharSequenceLowercase());
-		pipeList.add(new CharSequence2TokenSequence(Pattern
-				.compile("\\p{L}[\\p{L}\\p{P}]+\\p{L}")));
-		pipeList.add(new TokenSequenceRemoveStopwords(new File(stopPath),
-				"UTF-8", false, false, false));
-		pipeList.add(new TokenSequence2FeatureSequence());
-
-		InstanceList instances = new InstanceList(new SerialPipes(pipeList));
 
 		// String filepath = "/home/spyros/Documents/data/yelp_full.mallet";
 		String filepath = "/tudelft.net/staff-bulk/ewi/insy/mmc/nathan/dim-data/yelp_full.mallet";
-		BufferedReader br = new BufferedReader(new FileReader(
-				new File(filepath)));
+		//BufferedReader br = new BufferedReader(new FileReader(
+		//		new File(filepath)));
 		String line;
 		Gson gson = new Gson();
-		Type malletInstanceType = new TypeToken<Instance>() {
-		}.getType();
+		Type malletInstanceType = new TypeToken<Instance>() {}.getType();
 
-		int counter = 0;
-		while ((line = br.readLine()) != null) {
-			Instance instance = gson.fromJson(line, malletInstanceType);
-			instances.addThruPipe(instance);
-			// break;
-			counter++;
-			if (counter == 1000) {
-				break;
+		File[] malletFiles = new File("").listFiles();
+		HashMap<String, InstanceList> allInstanceLists = new HashMap<String, InstanceList>();
+		HashMap<String, ParallelTopicModel> allParallelTopicModels = new HashMap<String, ParallelTopicModel>();
+		
+		String[] categoriesArray = {"Restaurants", "Nightlife", "Bars", "Arts_&_Entertainment", "Hotels_&_Travel", "Shopping", "Casinos", "Beauty_&_Spas", "Automotive", "Health_&_Medical", "Home_Services", "Performing_Arts","Grocery", "Fashion", "Fitness_&_Instruction"};
+		ArrayList<String> allCategories = new ArrayList<String>();
+		allCategories.addAll(Arrays.asList(categoriesArray));
+		
+		// read all files and create the InstanceLists
+		for (File malletFile : malletFiles){
+			
+			// get the category from the file name
+			String currentCategory = malletFile.getName().split(".")[1];
+			if(!allCategories.contains(currentCategory)){
+				continue;
 			}
-		}
-		br.close();
-
-		// Create a model with 100 topics, alpha_t = 0.01, beta_w = 0.01
-		// Note that the first parameter is passed as the sum over topics, while
-		// the second is the parameter for a single dimension of the Dirichlet
-		// prior.
-		int numTopics = 100;
-		ParallelTopicModel model = new ParallelTopicModel(numTopics, 1.0, 0.01);
-
-		model.addInstances(instances);
-
-		// Use two parallel samplers, which each look at one half the corpus and
-		// combine
-		// statistics after every iteration.
-		model.setNumThreads(2);
-
-		// Run the model for 50 iterations and stop (this is for testing only,
-		// for real applications, use 1000 to 2000 iterations)
-		model.setNumIterations(50);
-		model.estimate();
-
-		// Show the words and topics in the first instance
-
-		// The data alphabet maps word IDs to strings
-		Alphabet dataAlphabet = instances.getDataAlphabet();
-
-		FeatureSequence tokens = (FeatureSequence) model.getData().get(0).instance
-				.getData();
-		LabelSequence topics = model.getData().get(0).topicSequence;
-
-		Formatter out = new Formatter(new StringBuilder(), Locale.US);
-		for (int position = 0; position < tokens.getLength(); position++) {
-			out.format("%s-%d ", dataAlphabet.lookupObject(tokens
-					.getIndexAtPosition(position)), topics
-					.getIndexAtPosition(position));
-		}
-		System.out.println(out);
-
-		// Estimate the topic distribution of the first instance,
-		// given the current Gibbs state.
-		double[] topicDistribution = model.getTopicProbabilities(0);
-
-		// Get an array of sorted sets of word ID/count pairs
-		ArrayList<TreeSet<IDSorter>> topicSortedWords = model.getSortedWords();
-
-		// Show top 5 words in topics with proportions for the first document
-		for (int topic = 0; topic < numTopics; topic++) {
-			Iterator<IDSorter> iterator = topicSortedWords.get(topic)
-					.iterator();
-
-			out = new Formatter(new StringBuilder(), Locale.US);
-			out.format("%d\t%.3f\t", topic, topicDistribution[topic]);
-			int rank = 0;
-			while (iterator.hasNext() && rank < 5) {
-				IDSorter idCountPair = iterator.next();
-				out.format("%s (%.0f) ",
-						dataAlphabet.lookupObject(idCountPair.getID()),
-						idCountPair.getWeight());
-				rank++;
+			
+			// create reader to read the file 
+			BufferedReader reader = new BufferedReader(new FileReader(malletFile));
+			
+			// store a new instance list instance in the map
+			allInstanceLists.put(currentCategory, TopicModeler.createInstanceList());
+			InstanceList currentInstanceList = allInstanceLists.get(currentCategory);
+			
+			int counter = 0;
+			
+			while ((line = reader.readLine()) != null) {
+				Instance instance = gson.fromJson(line, malletInstanceType);
+				currentInstanceList.addThruPipe(instance);
+				
+				counter++;
+				if (counter == 5000) {
+					break;
+				}
 			}
-			System.out.println(out);
+			
+			reader.close();
+			
+			
+			
+			// Create a model with 100 topics, alpha_t = 0.01, beta_w = 0.01
+			// Note that the first parameter is passed as the sum over topics, while
+			// the second is the parameter for a single dimension of the Dirichlet
+			// prior.
+			int numTopics = 20;
+			ParallelTopicModel currentModel = new ParallelTopicModel(numTopics, 1.0, 0.01);
+			allParallelTopicModels.put(currentCategory, currentModel);
+			currentModel.addInstances(currentInstanceList);
+			
+			// Use two parallel samplers, which each look at one half the corpus and
+			// combine
+			// statistics after every iteration.
+			currentModel.setNumThreads(2);
+			
+			
+			// Run the model for 50 iterations and stop (this is for testing only,
+			// for real applications, use 1000 to 2000 iterations)
+			currentModel.setNumIterations(50);
+			currentModel.estimate();
+			
 		}
-
-		// Create a new instance with high probability of topic 0
-		StringBuilder topicZeroText = new StringBuilder();
-		Iterator<IDSorter> iterator = topicSortedWords.get(0).iterator();
-
-		int rank = 0;
-		while (iterator.hasNext() && rank < 5) {
-			IDSorter idCountPair = iterator.next();
-			topicZeroText.append(dataAlphabet.lookupObject(idCountPair.getID())
-					+ " ");
-			rank++;
-		}
-
-		// Create a new instance named "test instance" with empty target and
-		// source fields.
-		InstanceList testing = new InstanceList(instances.getPipe());
-		testing.addThruPipe(new Instance(topicZeroText.toString(), null,
-				"test instance", null));
-
-		TopicInferencer inferencer = model.getInferencer();
-		double[] testProbabilities = inferencer.getSampledDistribution(
-				testing.get(0), 10, 1, 5);
-		System.out.println("0\t" + testProbabilities[0]);
 	}
 
 }
